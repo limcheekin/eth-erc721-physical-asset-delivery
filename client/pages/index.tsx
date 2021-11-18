bbeb022c08484d7c253f514338f4a8f634a4b788
Lim Chee Kin <limcheekin@vobject.com>
2021-11-18 20:31:28 +0800

    feat: added skynet deployment and updated readme

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Component } from 'react'
 import Layout from '../components/Layout'
@@ -18,6 +17,7 @@ function Home() {
         <title>{title}</title>
         <meta name="description" content={title} />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
 
       <main className={styles.main}>
@@ -42,19 +42,12 @@ function Home() {
 
       <footer className={styles.footer}>
         Powered by{' '}
-        <span className={styles.logo}>
-          <Image src="/logos/ethereum.png" alt="Ethereum Logo" width={144} height={32} />
-        </span>
-        <span className={styles.logo}>
-          <Image src="/logos/nextjs.png" alt="NextJS Logo" width={64} height={32} />
-        </span>
-        <span className={styles.logo}>
-          <Image src="/logos/metamask.png" alt="MetaMask Logo" width={128} height={32} />
-        </span>
-        <span className={styles.logo}>
-          <Image src="/logos/walletconnect.png" alt="WalletConnect Logo" width={128} height={32} />
-        </span>      
-      </footer>
+        <img className={styles.logo} src="/logos/ethereum.png" alt="Ethereum Logo" width="144" height="32" />
+        <img className={styles.logo} src="/logos/nextjs.png" alt="NextJS Logo" width="64" height="32" />
+        <img className={styles.logo} src="/logos/metamask.png" alt="MetaMask Logo" width="128" height="32" />
+        <img className={styles.logo} src="/logos/walletconnect.png" alt="WalletConnect Logo" width="128" height="32" />
+        { process.env.NEXT_PUBLIC_GITHUB_RUN_NUMBER && <div className={styles.logo}>b{process.env.NEXT_PUBLIC_GITHUB_RUN_NUMBER}</div> }
+      </footer>    
     </Layout>
   )
 }
